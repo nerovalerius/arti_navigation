a5b98110aa30551c2d0187d8f01c28eada4b9504
nerovalerius <crypto@mailbox.org>
2021-01-12 16:47:14 +0100

    added bash script for automated bag cartographer

--- cartographer_ros/configuration_files/arti_2d.lua
+++ cartographer_ros/configuration_files/arti_2d.lua
@@ -24,7 +24,7 @@ options = {
   odom_frame = "odom",
   provide_odom_frame = true,
   publish_frame_projected_to_2d = false,
-  use_odometry = true,
+  use_odometry = true,                      -- SLAM completely fails without odometry data
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans = 0,
@@ -45,15 +45,55 @@ options = {
 MAP_BUILDER.use_trajectory_builder_2d = true
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.use_imu_data = true
+
+-- Use the IMU data from Ouster - does not seem to work properly - it is optional for 2D anyway
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.15
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(35.)
-TRAJECTORY_BUILDER_2D.min_range = 1
-TRAJECTORY_BUILDER_2D.imu_gravity_time_constant = 1
+
+-- Bandpass for 3D Point Clouds - Distance in meters
+-- Localization fails of the range is too small, e.g from 0.5 to 1
+TRAJECTORY_BUILDER_2D.min_range = 0.5
+TRAJECTORY_BUILDER_2D.max_range = 4
+
+TRAJECTORY_BUILDER_2D.imu_gravity_time_constant = .1
 
 POSE_GRAPH.optimization_problem.huber_scale = 1e2
 
+-- turn off global SLAM to not mess with tuning of local SLAM
+POSE_GRAPH.optimize_every_n_nodes = 0
+
+-- voxel size -- original value: 0.025
+-- value 0.5 is too high - result gets blurred
+ TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.025
+
+-- size of submaps
+-- TRAJECTORY_BUILDER_2D.submaps.num_range_data = 
+
+-- Number of 3D Images for a single scan
+-- TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
+
+--TRAJECTORY_BUILDER_2D.imu_gravity_time_constant = .1
+
+-- Costs for moving the result away from the prior 
+-- scan matching has to generate a higher score in another position to be accepted
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 1e2
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 4e2
+
+-- individual weights of local SLAM and odometry
+-- POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1
+-- POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1
+-- POSE_GRAPH.optimization_problem.odometry_translation_weight = 1
+-- POSE_GRAPH.optimization_problem.odometry_rotation_weight = 1
+
+-- Set Threads to 4 
+MAP_BUILDER.num_background_threads = 4
+
+-- T
+
+
+
 return options
 
---TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
---TRAJECTORY_BUILDER_2D.imu_gravity_time_constant = .1
+
